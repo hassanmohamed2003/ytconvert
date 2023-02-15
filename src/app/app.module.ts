f308dd35166c76f77601bb192c3b803657b80572
hassanm <s1136140@student.hsleiden.nl>
2023-02-15 05:44:46 +0100

    ✨:added styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DownloadpageComponent } from './downloadpage/downloadpage.component';
+import { DownloadComponent } from './downloadpage/download/download.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DownloadpageComponent,
+    DownloadComponent
   ],
   imports: [
     BrowserModule
